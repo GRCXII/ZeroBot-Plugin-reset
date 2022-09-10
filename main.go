c906af3ac62f3fbd9fbf2531c47eed1ed62fbc10
GRCXII <99615877+GRCXII@users.noreply.github.com>
2022-09-11 06:28:25 +0800

    reset/port

--- main.go
+++ main.go
@@ -181,9 +181,9 @@ func init() {
 	w := flag.Bool("w", false, "Enable warning level log and higher.")
 	h := flag.Bool("h", false, "Display this help.")
 	// 直接写死 AccessToken 时，请更改下面第二个参数
-	token := flag.String("t", "", "Set AccessToken of WSClient.")
+	token := flag.String("t", "1deafult2Token", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://127.0.0.1:45555", "Set Url of WSClient.")
+	url := flag.String("u", "ws://127.0.0.1:55555", "Set Url of WSClient.")
 	// 默认昵称
 	adana := flag.String("n", "IABOT", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
